47b6ed82f2f8b7a34e74957d561c5cec4b430329
GorGre14 <93708520+GorGre14@users.noreply.github.com>
2026-07-30 21:33:37 +0200

    Prilozena knjiznica MPJ Express in poenotene zagonske skripte
    Projekt je sedaj samostojen: MPJ Express je v lib/mpj, zato dodatna
    namestitev ni potrebna in projekt se prevede takoj po razpakiranju.
    
    - setup_env.sh poisce JAVA_HOME in MPJ_HOME ter popravi pravice MPJ skript
    - pom.xml uporablja ${mpj.home} s privzeto vrednostjo ${project.basedir}/lib/mpj
      namesto absolutne poti
    - vse tri zagonske skripte sprejemajo enake opcije

--- run_distributed.sh
+++ run_distributed.sh
@@ -1,55 +1,28 @@
 #!/bin/bash
-# Zagon porazdeljene simulacije z MPJ Express
-# Zahteva: MPJ Express mora biti namescen in MPJ_HOME nastavljen
+# Zagon porazdeljene simulacije (MPJ Express).
 #
-# Uporaba: ./run_distributed.sh <stevilo_procesov> [--particles N] [--cycles N]
-# Primer: ./run_distributed.sh 4 --particles 500 --cycles 1000
-
-# Avtomatsko zaznavanje JAVA_HOME na macOS, ce ni nastavljen
-if [ -z "$JAVA_HOME" ]; then
-    if [ -d "/opt/homebrew/opt/openjdk/libexec/openjdk.jdk/Contents/Home" ]; then
-        export JAVA_HOME="/opt/homebrew/opt/openjdk/libexec/openjdk.jdk/Contents/Home"
-        export PATH="$JAVA_HOME/bin:$PATH"
-    fi
-fi
+# Uporaba: ./run_distributed.sh <stevilo_procesov> [opcije]
+# Primer:  ./run_distributed.sh 4 --particles 1000 --cycles 2000 --ui false
+set -e
+cd "$(dirname "$0")"
+source ./setup_env.sh
 
 if [ -z "$MPJ_HOME" ]; then
-    if [ -d "$HOME/mpj" ]; then
-        export MPJ_HOME="$HOME/mpj"
-    elif [ -d "/Users/gregorantonaz/mpj" ]; then
-        export MPJ_HOME="/Users/gregorantonaz/mpj"
-    elif [ -d "/usr/local/mpj" ]; then
-        export MPJ_HOME="/usr/local/mpj"
-    elif [ -d "/opt/mpj" ]; then
-        export MPJ_HOME="/opt/mpj"
-    else
-        echo "NAPAKA: MPJ_HOME ni nastavljen."
-        echo "Namestite MPJ Express in nastavite: export MPJ_HOME=/pot/do/mpj"
-        exit 1
-    fi
-fi
-
-if [ -z "$1" ]; then
-    echo "Uporaba: ./run_distributed.sh <stevilo_procesov> [opcije]"
-    echo "Primer: ./run_distributed.sh 4 --particles 500 --cycles 1000"
+    echo "NAPAKA: MPJ Express ni najden."
+    echo "Namestite ga in nastavite: export MPJ_HOME=/pot/do/mpj"
     exit 1
 fi
 
-# Preverjanje, ali je prvi argument stevilo
 if [[ ! "$1" =~ ^[0-9]+$ ]]; then
-    echo "NAPAKA: Prvi argument mora biti celo število (število procesov)."
     echo "Uporaba: ./run_distributed.sh <stevilo_procesov> [opcije]"
-    echo "Primer: ./run_distributed.sh 4 --particles 500 --cycles 1000"
+    echo "Primer:  ./run_distributed.sh 4 --particles 1000 --cycles 2000"
     exit 1
 fi
-
 NP=$1
 shift
 
-# Prevajanje
 mvn -q compile
 
-# Zagon z mpjrun
-$MPJ_HOME/bin/mpjrun.sh -np $NP \
-  -cp target/classes:$MPJ_HOME/lib/mpj.jar \
+"$MPJ_HOME/bin/mpjrun.sh" -np "$NP" \
+  -cp "target/classes:$MPJ_HOME/lib/mpj.jar" \
   com.example.chargedparticles.DistributedRunner "$@"
